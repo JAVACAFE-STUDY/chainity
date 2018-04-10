fb7074ca37f72bed991461d5713684c4cd3cbdba
Choi Cihwan <clghks@gmail.com>
2018-04-11 01:56:57 +0900

    UI 에서 입력 받도록 수정

--- public/js/transaction.js
+++ public/js/transaction.js
@@ -1,12 +1,12 @@
 
 function sendTranaction() {
-  $.get("/sendTransaction?from=0xA5C4B67A464AA5A511f0C8B360b2e8Ad83a49A06&to=0xE125665F3aDb9BA3013E218057C891dd2d79Ee8a&value=100", function(data){
-    $("#balance_of_account0").text(data);
+  $.get("/sendTransaction?from=" + $("#fromAddress").val() + "&to=" + $("#toAddress").val() + "&value=" + $("#transactionValue").val(), function(data){
+    console.log(data);
   });
 }
 
 function sendApprove() {
-  $.get("/sendApprove?spender=0xA5C4B67A464AA5A511f0C8B360b2e8Ad83a49A06&value=100", function(data){
-    $("#balance_of_account0").text(data);
+  $.get("/sendApprove?spender=" + $("#senderAddress").val() + "&value=" + $("#senderValue").val(), function(data){
+    console.log(data);
   });
 }
