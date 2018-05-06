e8285e8545e58ad1265c85977a9772eaa66f459e
CHO <rygh4775@gmail.com>
2018-05-06 21:47:58 +0900

    frontend 추

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+Vue.prototype.$http = axios
 
 Vue.use(BootstrapVue)
 
