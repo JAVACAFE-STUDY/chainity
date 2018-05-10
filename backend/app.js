2316a68363b91f17a860d2f0b45fc5870e39edc7
CHO <rygh4775@gmail.com>
2018-05-11 00:40:17 +0900

    해당 호출 api 없을 시 fallback 하는 기능 위치 변경

--- backend/app.js
+++ backend/app.js
@@ -26,11 +26,11 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use(require('connect-history-api-fallback')());
-
 // API
 app.use('/api/users', users);
 app.use('/api/issues', issues);
+
+app.use(require('connect-history-api-fallback')());
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
