600b58dd24d49d1cc3cf1b4b08a0c80555c3666b
CHO <rygh4775@gmail.com>
2018-05-11 00:39:34 +0900

    view engine 누락 추

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,10 @@ var users = require('./api/users');
 var issues = require('./api/issues');
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 // Middlewares
 app.use(logger('dev'));
 app.use(bodyParser.json());
