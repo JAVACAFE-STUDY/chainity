f146a7dbde53e50c51321ab09f1d93cddf91db08
CHO <rygh4775@gmail.com>
2018-05-12 19:52:18 +0900

    (frontend) 로그인 기능 구현 (추후 DB 연동 필요)

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -6,24 +6,26 @@
           <b-card-group>
             <b-card no-body class="p-4">
               <b-card-body>
-                <h1>Login</h1>
-                <p class="text-muted">Sign In to your account</p>
-                <b-input-group class="mb-3">
-                  <b-input-group-prepend><b-input-group-text><i class="icon-user"></i></b-input-group-text></b-input-group-prepend>
-                  <input type="text" class="form-control" placeholder="Username">
-                </b-input-group>
-                <b-input-group class="mb-4">
-                  <b-input-group-prepend><b-input-group-text><i class="icon-lock"></i></b-input-group-text></b-input-group-prepend>
-                  <input type="password" class="form-control" placeholder="Password">
-                </b-input-group>
-                <b-row>
-                  <b-col cols="6">
-                    <b-button variant="primary" class="px-4">Login</b-button>
-                  </b-col>
-                  <b-col cols="6" class="text-right">
-                    <b-button variant="link" class="px-0">Forgot password?</b-button>
-                  </b-col>
-                </b-row>
+                <b-form novalidate="novalidate" class="was-validated" @submit="onSubmit">
+                  <h1>Login</h1>
+                  <p class="text-muted">Sign In to your account</p>
+                  <b-input-group class="mb-3">
+                    <b-input-group-prepend><b-input-group-text><i class="icon-user"></i></b-input-group-text></b-input-group-prepend>
+                    <input v-model="form.email" type="text" class="form-control" placeholder="Username">
+                  </b-input-group>
+                  <b-input-group class="mb-4">
+                    <b-input-group-prepend><b-input-group-text><i class="icon-lock"></i></b-input-group-text></b-input-group-prepend>
+                    <input v-model="form.password" type="password" class="form-control" placeholder="Password">
+                  </b-input-group>
+                  <b-row>
+                    <b-col cols="6">
+                      <b-button type="submit" variant="primary" class="px-4">Login</b-button>
+                    </b-col>
+                    <b-col cols="6" class="text-right">
+                      <b-button variant="link" class="px-0">Forgot password?</b-button>
+                    </b-col>
+                  </b-row>
+                </b-form>
               </b-card-body>
             </b-card>
           </b-card-group>
@@ -35,6 +37,32 @@
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data() {
+    return {
+      form: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault()
+      if(this.form.email != '' && this.form.password != '') {
+        this.$http.post('/api/auth/login', this.form)
+          .then((response) => {
+            if (response.status === 200) {
+              this.$session.start()
+              this.$session.set('user-token', response.data.data)
+              this.$http.defaults.headers.common['x-access-token'] = response.data.data;
+              this.$router.push('/')
+            }
+          }).catch((error) => {
+            alert(error.response.data.message)
+          })
+      }
+    }
+  }
 }
 </script>
