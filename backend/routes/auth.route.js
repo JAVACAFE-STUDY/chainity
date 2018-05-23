433fc9ce21fcf0f3716b464c75f2e20cc930d99b
CHO <rygh4775@gmail.com>
2018-05-24 02:32:52 +0900

    (backend) 회원가입 API 추가

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -3,6 +3,7 @@ var validate = require('express-validation');
 var expressJwt = require('express-jwt');
 var paramValidation = require('../config/param-validation');
 var authCtrl = require('../controllers/auth.controller');
+var userCtrl = require('../controllers/user.controller');
 var config = require('../config/config');
 
 const router = express.Router(); // eslint-disable-line new-cap
@@ -10,6 +11,10 @@ const router = express.Router(); // eslint-disable-line new-cap
 /** POST /api/auth/login - Returns token if correct username and password is provided */
 router.route('/login')
   .post(validate(paramValidation.login), authCtrl.login);
+
+/** POST /api/auth/register - Returns token if correct username and password is provided */
+router.route('/register')
+  .post(validate(paramValidation.register), authCtrl.loadUserToRegister, userCtrl.update);
 
 /** GET /api/auth/random-number - Protected route,
  * needs token returned by the above as header. Authorization: Bearer {token} */
