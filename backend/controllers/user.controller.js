24995d17fbbccaf45f6743919f3882e4e463eefe
CHO <rygh4775@gmail.com>
2018-05-23 23:14:51 +0900

    (backend) system 계정 없을 시 자동 생성

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,22 @@
 var User = require('../models/user.model');
 
+User.list()
+    .then(users => {
+      if (users.length < 1) {
+        const user = new User({
+          email: 'system',
+          name: 'system',
+          status: 'active',
+          role: 'admin'
+        });
+        
+        user.save()
+          .then(savedUser => console.info(savedUser))
+          .catch(e => console.error);
+      }
+    })
+    .catch(e => console.error);
+
 /**
  * Load user and append to req.
  */
@@ -23,18 +40,15 @@ function get(req, res) {
 /**
  * Create new user
  * @property {string} req.body.name - The name of user.
- * * @property {string} req.body.email - The email of user.
+ * @property {string} req.body.email - The email of user.
  * @returns {User}
  */
 function create(req, res, next) {
 
-  //TODO create keyStore with password
-  var keyStore = {"address":"7cef57fd7faa78c4132e7c748115528e187042a4","crypto":{"cipher":"aes-128-ctr","ciphertext":"4d22f75ebcf79a0d30f4aa470a83fa13d1071fd80ed87c937dba894697963c27","cipherparams":{"iv":"60d4902492dab3a514da5d92e3b16bff"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"847825e6ebcea8e0f9a38d1c06e01470cc74153d8ae7a599cd68466f209d6391"},"mac":"819da9b375c62413c59d6c15804bc5b3fa249a852f968560939f7646e2ca82f1"},"id":"bc933aa4-7772-423b-aa49-368126d673f3","version":3}
-
   const user = new User({
-    name: req.body.name,
     email: req.body.email,
-    keyStore: keyStore
+    role: req.body.role,
+    status: req.body.status
   });
 
   user.save()
