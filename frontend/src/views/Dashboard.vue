3eec1986600db7c1884428a88d195389ef3111d2
CHO <rygh4775@gmail.com>
2018-07-11 00:12:11 +0900

    add(frontend) : 대시보드에 tags-input 샘플 구현 (추후 제거 필요)

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,11 +1,53 @@
 <template>
   <div class="animated fadeIn">
     <p class="text-info">Hello World</p>
+    <vue-tags-input
+      class="tags-input"
+      v-model="tag"
+      :tags="tags"
+      :allow-edit-tags="true"
+      :add-only-from-autocomplete="true"
+      @tags-changed="newTags => tags = newTags"
+      :autocomplete-items="items">
+      <div slot="tagCenter" slot-scope="props">
+        <span
+          @click="props.performOpenEdit(props.index)"
+          v-if="!props.edit">{{ props.tag.text }}
+        </span>
+        <div class="inputs" v-else>
+          <select
+            v-model="props.tag.text"
+            @change="props.validateTag(props.index)">
+            <option v-for="(animal, index) in animals" :key="index">{{ animal }}</option>
+          </select>
+          <i class="material-icons" @click="props.performSaveTag(props.index)">check</i>
+        </div>
+      </div>
+    </vue-tags-input>
   </div>
 </template>
 
 <script>
+import VueTagsInput from '@johmun/vue-tags-input';
+
 export default {
-  name: 'dashboard'
+  name: 'dashboard',
+  components: {VueTagsInput},
+  data () {
+    return {
+     animals: [
+        'Lion', 'Turtle', 'Rabbit', 'Frog', 'Squirrel', 'Owl', 'Bee',
+      ],
+      tag: '',
+      tags: []
+    }
+  },
+  computed: {
+    items() {
+      return this.animals
+        .filter(a => new RegExp(this.tag, 'i').test(a))
+        .map(a => ({ text: a }));
+    }
+  }
 }
 </script>
