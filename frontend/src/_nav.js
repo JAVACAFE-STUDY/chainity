b8876721ca5a9e0eb27d6c44d14a61ba93bf5da1
CHO <rygh4775@gmail.com>
2018-08-18 11:26:55 +0900

    refact(frontend): URL 경로 수정 및 한글화

--- frontend/src/_nav.js
+++ frontend/src/_nav.js
@@ -6,7 +6,7 @@ export default {
       icon: 'icon-speedometer'
     },
     {
-      name: 'Issues',
+      name: '이슈',
       url: '/issues',
       icon: 'icon-list'
     },
@@ -16,9 +16,21 @@ export default {
       icon: 'icon-speedometer'
     },
     {
-      name: 'Users',
+      name: '사용자',
       url: '/users',
       icon: 'icon-people'
+    },
+    {
+      name: '관리자메뉴',
+      url: '/admin',
+      icon: 'icon-puzzle',
+      children: [
+        {
+          name: '토큰충전요청',
+          url: '/admin/tokens-requests',
+          icon: 'icon-puzzle'
+        }
+      ]
     }
   ]
 }
