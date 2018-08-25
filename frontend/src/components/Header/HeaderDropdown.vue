dc284a628e7f048e9c00fcec3f3b3cd9e0fc432e
유미령 <kioku714@gmail.com>
2018-08-25 15:43:16 +0900

    헤더 드롭다운 메뉴 한글화 완료.

--- frontend/src/components/Header/HeaderDropdown.vue
+++ frontend/src/components/Header/HeaderDropdown.vue
@@ -3,11 +3,11 @@
         <template slot="button-content">
           <img src="static/img/avatars/6.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
         </template>
-        <b-dropdown-header tag="div" class="text-center"><strong>Settings</strong></b-dropdown-header>
-        <b-dropdown-item @click="profile"><i class="fa fa-user"></i> Profile</b-dropdown-item>
+        <b-dropdown-header tag="div" class="text-center"><strong>설정</strong></b-dropdown-header>
+        <b-dropdown-item @click="profile"><i class="fa fa-user"></i> 프로필</b-dropdown-item>
         <b-dropdown-item @click="payments"><i class="fa fa-usd"></i> 토큰 관리<b-badge variant="secondary">{{itemsCount}}</b-badge></b-dropdown-item>
         <b-dropdown-divider></b-dropdown-divider>
-        <b-dropdown-item @click="logout"><i class="fa fa-lock"></i> Logout</b-dropdown-item>
+        <b-dropdown-item @click="logout"><i class="fa fa-lock"></i> 로그아웃</b-dropdown-item>
       </b-nav-item-dropdown>
 </template>
 <script>
