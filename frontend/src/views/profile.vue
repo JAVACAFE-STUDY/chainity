af481738b46765a5bb84bfc3be8e4123d8e23b5a
CHO <rygh4775@gmail.com>
2018-09-21 21:28:50 +0900

    add(frontend) : user 프로필 사진 크롭핑(?) 업로드

--- frontend/src/views/profile.vue
+++ frontend/src/views/profile.vue
@@ -5,13 +5,11 @@
         <h1>프로필</h1>
       </div>
       <b-row>
-        <b-col cols="5">
-          <div class="card-body p-4">
+          <!-- <div class="card-body p-4">
             <b-img center fluid :src="previewData" onerror="this.onerror=null;this.src='static/img/avatars/profile.jpg';" alt="center image" />
             <br>
             <input type="file" @change="previewImage" accept="image/*">
-          </div>
-        </b-col>
+          </div> -->
         <b-col cols="7">
           <div class="card-body p-4">
             <div class="input-group mb-3">
@@ -20,14 +18,14 @@
                   <i class="icon-user"></i>
                 </span>
               </div>
-              <input type="text" class="form-control" v-model="item.name" placeholder="Username">
+              <input type="text" class="form-control" v-model="user.name" placeholder="Username">
             </div>
 
             <div class="input-group mb-3">
               <div class="input-group-prepend">
                 <span class="input-group-text">@</span>
               </div>
-              <input type="text" class="form-control" v-model="item.email" placeholder="Email" readonly>
+              <input type="text" class="form-control" v-model="user.email" placeholder="Email" readonly>
             </div>
 
             <div class="input-group mb-3">
@@ -45,7 +43,7 @@
                   <i class="icon-home"></i>
                 </span>
               </div>
-              <input type="text" class="form-control" v-model="item.keyStore.address" readonly>
+              <input type="text" class="form-control" :value="user.keyStore ? '0x'+user.keyStore.address : ''" readonly>
             </div>
 
             <div class="input-group mb-3">
@@ -54,78 +52,84 @@
                   <i class="icon-people"></i>
                 </span>
               </div>
-              <input type="text" class="form-control" v-model="item.role" placeholder="Role" readonly>
+              <input type="text" class="form-control" v-model="user.role" placeholder="Role" readonly>
             </div>
 
             <button type="button" v-on:click="onSubmit" class="btn btn-block btn-success">수정</button>
           </div>
         </b-col>
+        <b-col cols="5">
+          <div class="card-body p-4">
+            <b-row>
+              <b-card show-footer>
+                <b-img width="200px" :src="$http.defaults.baseURL + '/api/images/' + user.avatar" alt="로딩중..." />
+                <div slot="header">프로필 사진</div>
+                <div slot="footer" class="text-sm-right">
+                  <b-button id="pick-avatar" block variant="success">프로필 사진 올리기</b-button>
+                </div>
+              </b-card>
+            </b-row>
+          </div>
+        </b-col>
       </b-row>
     </div>
+    <avatar-cropper
+              @uploaded="handleUploaded"
+              trigger="#pick-avatar"
+              :labels="{submit: '완료', cancel: '취소'}"
+              :upload-headers="{Authorization: 'Bearer '+$session.get('user-token')}"
+              :upload-url="$http.defaults.baseURL+'/api/users/me/images/profile'" />
   </div>
 </template>
 
 <script>
 
+import AvatarCropper from 'vue-avatar-cropper'
+
 export default {
   name: 'Profile',
+  components: { AvatarCropper },
   data: () => {
     return {
-      item: [],
+      user: {},
       tokens: 0,
-      previewData: null,
-      imageData: null
+      avatarUrl: 'static/img/avatars/profile.jpg',
+      imageData: null,
+      avatar: {
+        labels: {
+          submit: '완료',
+          cancel: '취소'
+        }
+      }
     }
   },
-  mounted: function () {
-    this.$http.get('/api/users/me')
+  created () {
+    this.fetchUser()
+    this.fetchToken()
+  },
+  methods: {
+    fetchUser() {
+      this.user = {}
+      this.$http.get('/api/users/me')
       .then((response) => {
-        this.item = response.data
-        this.item.keyStore.address = '0x' + response.data.keyStore.address
-        this.previewData = 'http://localhost:3000/api/images/' + this.item._id + '/profile'
+        this.user = response.data
       })
-
-    this.$http.get('/api/users/me/tokens')
+    },
+    fetchToken() {
+      this.$http.get('/api/users/me/tokens')
       .then((response) => {
         this.tokens = response.data.tokens + ' JC'
       })
-  },
-  methods: {
-    onSubmit: function (event) {
-      if (this.imageData == null) {
-        this.updateProfile()
-      } else {
-        const formData = new FormData()
-        formData.append('profile', this.imageData[0], this.item._id)
-        this.$http.post('/api/users/' + this.item._id + '/images/profile', formData)
-          .then((response) => {
-            if (response.data.result === 'Success') {
-              this.updateProfile()
-            } else {
-              alert('업데이트 실패')
-            }
-          })
-      }
     },
-    previewImage: function (event) {
-      var input = event.target
-      if (input.files && input.files[0]) {
-        this.imageData = event.target.files
-        var reader = new FileReader()
-        reader.onload = (e) => {
-          this.previewData = e.target.result
-        }
-        reader.readAsDataURL(input.files[0])
-      }
+    handleUploaded(resp) {
+      this.$toastr.s('프로필 사진 업데이트 완료')
+      this.$eventHub.$emit('thumbnail-changed')
+      this.fetchUser()
     },
-    updateProfile: function () {
-      this.$http.put('/api/users/' + this.item._id, this.item)
+    onSubmit(event) {
+      this.$http.put('/api/users/' + this.user._id, this.user)
         .then((response) => {
-          if (response.status === 200) {
-            alert('업데이트 완료')
-          } else {
-            alert('업데이트 실패')
-          }
+          this.$toastr.s('프로필 업데이트 완료')
         })
     }
   }
