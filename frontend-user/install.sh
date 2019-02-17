ad4f4ffa7dbc35ea0f30aabd45723eef6510222e
whybe <youngbeomrhee@gmail.com>
2019-02-17 20:53:06 +0900

    React 환경설정

---
+++ frontend-user/install.sh
@@ -0,0 +1,16 @@
+sudo npm i webpack -g;
+
+npm init;
+
+npm i react react-dom;
+
+npm i --save-dev babel-core babel-loader babel-preset-env babel-preset-react babel-preset-stage-0 opener;
+
+# css 번들하기
+npm i style-loader css-loader postcss-loader autoprefixer --save-dev;
+
+# hot loader
+npm i --save-dev
+
+
+npm start;
