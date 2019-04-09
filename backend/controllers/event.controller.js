2982d83aa02730e993005125d396ae28a344e8a2
유미령 <kioku714@gmail.com>
2019-04-09 19:46:00 +0900

    add: 이벤트 참여 신청 조회 API 추가

---
+++ backend/controllers/event.controller.js
@@ -0,0 +1,177 @@
+var Event = require('../models/event.model');
+var Participation = require('../models/participation.model');
+var config = require('../config/config');
+var Tx = require('ethereumjs-tx');
+var Web3 = require('web3');
+
+var web3 = new Web3(new Web3.providers.HttpProvider(config.web3Provider));
+var erc20 = new web3.eth.Contract(JSON.parse(config.contractABI), config.contractAccount);
+var ObjectId = (require('mongoose').Types.ObjectId);
+
+/**
+ * Get event list.
+ * @property {number} req.query.offset - Number of events to be skipped.
+ * @property {number} req.query.limit - Limit number of events to be returned.
+ * @returns {Event[]}
+ */
+async function list(req, res, next) {
+  const { limit = 0, offset = 0 } = req.query;
+
+  const result = {
+    offset: req.query.offset,
+    limit: req.query.limit,
+    totalDocs: await Event.count(),
+    docs: await Event.list({ limit, offset })
+  };
+
+  res.json(result);
+}
+
+/**
+ * Create new event
+ * @property {string} req.body.title
+ * @property {string} req.body.description
+ * @property {string} req.body.tokens
+ * @property {string} req.body.maxNumberOfParticipants
+ * @property {string} req.body.startDate
+ * @property {string} req.body.finishDate
+ * @property {string} req.body.participants
+ * @returns {Event}
+ */
+function create(req, res, next) {
+  const event = new Event({
+    title: req.body.title,
+    description: req.body.description,
+    tokens: req.body.tokens,
+    maxNumberOfParticipants: req.body.maxNumberOfParticipants,
+    startDate: req.body.startDate,
+    finishDate: req.body.finishDate,
+    createdBy: req.decoded._id,
+    eventType: req.body.eventType,
+    isClosed: false,
+    createdAt: Date.now()
+  });
+
+  event.save()
+    .then(savedEvent=> res.json(savedEvent))
+    .catch(e => next(e));
+}
+
+/**
+ * Load event and append to req.
+ */
+function load(req, res, next, eventId) {
+  Event.get({ _id: new ObjectId(req.params.eventId) })
+    .then((event) => {
+      req.event = event;
+      return next();
+    }) 
+    .catch(e => next(e));
+}
+
+/**
+ * Get event
+ * @returns {Event}
+ */
+function get(req, res) {
+  return res.json(req.event);
+}
+
+/**
+ * Update existing event
+ */
+function update(req, res, next) {
+  const event = new Event(req.event);
+  if (req.body.title != '') {
+    event.title = req.body.title;
+  }
+  if (req.body.description != '') {
+    event.description = req.body.description;
+  }
+  if (req.body.maxNumberOfParticipants != '') {
+    event.maxNumberOfParticipants = req.body.maxNumberOfParticipants;
+  }
+  if (req.body.startDate != '') {
+    event.startDate = req.body.startDate;
+  }
+  if (req.body.finishDate != '') {
+    event.finishDate = req.body.finishDate;
+  }
+  if (req.body.isClosed != '') {
+    event.isClosed = req.body.isClosed;
+  }
+  if (req.body.closedAt != '') {
+    event.closedAt = req.body.closedAt;
+  }
+
+  Event.update({ _id: event._id}, event)
+    .then(savedEvent => res.json(savedEvent))
+    .catch(e => next(e));
+}
+
+/**
+ * Delete event.
+ * @returns {Event}
+ */
+function remove(req, res, next) {
+  const event = req.event;
+  event.remove()
+    .then(deletedEvent => res.json(deletedEvent))
+    .catch(e => next(e));
+}
+
+/**
+ * Get participations list.
+ * @property {number} req.query.offset - Number of events to be skipped.
+ * @property {number} req.query.limit - Limit number of events to be returned.
+ * @returns {Participation[]}
+ */
+async function getEventParticipations(req, res, next) {
+  const { limit = 0, offset = 0 } = req.query;
+  const q = { 'event' : req.event._id };
+  
+  const result = {
+    offset: req.query.offset,
+    limit: req.query.limit,
+    totalDocs: await Participation.count(q),
+    docs: await Participation.list({ limit, offset, q })
+  };
+
+  res.json(result);
+}
+/**
+ * Add participation
+ * @property {string} req.event._id
+ * @property {string} req.params.participationId
+ * @property {string} req.event.createdBy
+ * @returns {Event}
+ */
+function addParticipation(req, res, next) {
+  const participation = new Participation({
+    event: req.event._id,
+    participant: new ObjectId(req.params.participationId),
+    createdAt: Date.now(),
+    createdBy: req.event.createdBy
+  });
+
+  participation.save()
+    .then(savedParticipation=> res.json(savedParticipation))
+    .catch(e => next(e));
+}
+
+/**
+ * Remove participation
+ * @property {string} req.event._id
+ * @property {string} req.params.participationId
+ * @returns {Event}
+ */
+function removeParticipation(req, res, next) {
+  const participant = new ObjectId(req.params.participationId);
+  const q = { event: req.event._id, participant: participant };
+
+  Participation.deleteOne(q)
+    .then(removeParticipation => res.json(removeParticipation))
+    .catch(e => next(e));
+}
+
+module.exports = { list, create, load, get, update, remove, getEventParticipations, addParticipation, removeParticipation };
---
+++ backend/models/event.model.js
@@ -0,0 +1,105 @@
+var Promise = require('bluebird');
+var mongoose = require('mongoose');
+var httpStatus = require('http-status');
+var APIError = require('../helpers/APIError');
+var config = require('../config/config');
+
+mongoose.connect('mongodb://' + config.mongo.host + ':' + config.mongo.port);
+
+/**
+ * Event Schema
+ */
+const EventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: false
+  },
+  tokens: {
+    type: Number,
+    required: true
+  },
+  maxNumberOfParticipants: {
+    type: Number,
+    required: false
+  },
+  startDate: {
+    type: Date,
+    required: false
+  },
+  finishDate: {
+    type: Date,
+    required: false
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  eventType: {
+    type: String,
+    required: true
+  },
+  isClosed: {
+    type: Boolean,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true
+  },
+  closedAt: {
+    type: Date,
+    required: false
+  }
+});
+
+/**
+ * Methods
+ */
+EventSchema.method({
+});
+
+/**
+ * Statics
+ */
+EventSchema.statics = {
+  /**
+   * @param {number} skip - Number of events to be skipped.
+   * @param {number} limit - Limit number of events to be returned.
+   * @returns {Promise<Event[]>}
+   */
+  list({ limit = 0, offset = 0 } = {}) {
+    return this.find()
+    //   .populate('createdBy')
+      .sort({ createdAt: -1 })
+      .limit(+limit)
+      .skip(+offset)
+      .exec();
+  },
+
+  /**
+   * Get event
+   * @param {ObjectId} id - The objectId of event.
+   * @returns {Promise<Event, APIError>}
+   */
+  get(q) {
+    return this.findOne(q)
+      .exec()
+      .then((event) => {
+        if (event) {
+          return event;
+        }
+        const err = new APIError('No such event exists!', httpStatus.NOT_FOUND);
+        return Promise.reject(err);
+      });
+  }
+};
+
+/**
+ * @typedef Event
+ */
+module.exports = mongoose.model('Event', EventSchema);
